44ad13ec54890863990cce47e176fb942a4e2388
Mike <evilredme@gmail.com>
2025-12-11 19:17:06 +0300

    Lab for refactoring

---
+++ LibraryCabinet.js
@@ -0,0 +1,15 @@
+import Library from "./Library.js";
+export default class LibraryCabinet extends Library {
+    constructor(famousBook, bookCount, haveFreeSpace, shelfCount){
+        super(famousBook, bookCount, haveFreeSpace);
+        this.shelfCount = shelfCount;
+    } 
+    get ShelfCount(){
+        return this.shelfCount;
+    }
+    set ShelfCount(shelfCount){
+        if(shelfCount >=0){
+            this.shelfCount = shelfCount;
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+import LibraryCabinet from "./LibraryCabinet.js";
+const cabinet1 = new LibraryCabinet("Romeo", 12, false, 4);
+const cabinet2 = new LibraryCabinet("Master", 32, true, 7);
+cabinet1.show();
+cabinet2.show(); 
+//Я установил в папку для запуска на локальном сервере http-server, он запускается по этой же команде по стандртному порту локалхоста - 8080
